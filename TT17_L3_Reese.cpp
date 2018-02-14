fcbff68d6d8505d4d5057440a46ab2deb0e66cdd
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-02-13 18:40:39 -0600

    refactored stuff

--- TT17_L3_Reese.cpp
+++ TT17_L3_Reese.cpp
@@ -1,3 +1,4 @@
+
 // Author: Lee Reese;  Course: COSC 1337 Spring 2018 TT17;  Instructor: Thayer
 // Lab: 3  Purpose:  Virtual math tutor provides problems to challenge user.
 
@@ -16,56 +17,87 @@ int main(){
        << "Good luck!" << endl;
 
   //Some variables
-  int question_type;
-  int answer;
-  int a;
-  int b;
   int user_answer;
+  int answer;
 
-  //seed
+  //Statistics
+  int qs_asked[4] = {0, 0, 0, 0};
+  int qs_correct[4] = {0, 0, 0, 0};
+
+  //Seed RNG
   srand(time(NULL));
+
   //Give problems
   bool done = false;
   while (!done){ 
-    //Generate question type (*,/,+, or -) and answer
-    question_type = rand() % 4;
+    //Generate question type (*, /, +, or -)
+    int question_type = rand() % 4;
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
     
-    question_type = 0;
-    //Multiplication
+    //Generate multiplication and division
     if (question_type == 0 || question_type == 1){
       //Generate values
-      answer = rand() % 100 + 1;
-      a = rand() % int(sqrt(answer)) + 1;
-      b = answer / a;
-      answer = a * b;
-      printf("a:%d b:%d answer:%d\n", a, b, answer);
-
-      //Deliver problem
-      printf("%d * %d = ", a, b);
-      cin >> user_answer;
+      c = rand() % 100 + 1;
+      a = rand() % int(sqrt(c)) + 1;
+      b = c / a;
+      c = a * b;
     }
     
-    //question_type = 2;
-    //Addition
+    //Generate add and subtract values
     if (question_type == 2 || question_type == 3){
       //Generate values
-      answer = rand() % 100 + 1;
-      a = rand() % answer + 1;
-      b = answer - a;
-      
-      //Deliver problem
-      printf("%d + %d = ", a, b);
-      cin >> user_answer;
+      c = rand() % 100 + 1;
+      a = rand() % c + 1;
+      b = c - a;
     }
 
-    if (user_answer < 0){
+    switch(question_type) {
+      case 0 :
+        printf("%d * %d = ", a, b);
+        answer = c;
+        cin >> user_answer;
+	break;
+
+      case 1 :
+        printf("%d / %d = ",c, b);
+        answer = a;
+        cin >> user_answer;
+	break;
+
+      case 2 :
+        printf("%d + %d = ", a, b);
+        answer = c;
+        cin >> user_answer;
+	break;
+
+      case 3 :
+        printf("%d - %d = ", c, b);
+        answer = a;
+        cin >> user_answer;
+	break;
+    }
+
+    //Quit tutor
+    if (user_answer >= 0){
+      ++qs_asked[question_type];
+      if (user_answer == answer)
+	++qs_correct[question_type];
+    }
+
+    //Update accumulators
+    else {
       done = true;
-    } else if (user_answer == answer) {
-      //Do something
-    } else {
-      //Do something else
+      
     }
   }
-  //calculate score
-  return 0;
+  //Calculate score
+  cout << "\nFinal Grades:" << endl;
+  printf("Multiplication:\t%d/%d correct.\n", qs_correct[0], qs_asked[0]);
+  printf("Division:\t%d/%d correct.\n", qs_correct[1], qs_asked[1]);
+  printf("Addition:\t%d/%d correct.\n", qs_correct[2], qs_asked[2]);
+  printf("Subtraction:\t%d/%d correct.\n", qs_correct[3], qs_asked[3]);
+
 }
