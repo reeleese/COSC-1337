6a17d8706edeec723969dd37ebe3d583f8407b9f
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-02-15 15:59:36 -0600

    Fixed enum and user input.

--- TT17_L4_Reese.cpp
+++ TT17_L4_Reese.cpp
@@ -13,26 +13,34 @@ TODO:
 using namespace std;
 
 int main() {
+  enum Material {air='a', water='w', steel='s', quit='q'};
+  
+  //Loop
   bool done = false;
   while (!done) {
     //Menu
     cout << "a)ir, w)water, s)teel or  q)uit" << endl;
-    cout << "Option";
+    cout << "Option: ";
 
     //Get option
-    enum Material {air='a', water='w', steel='s', quit='q'};
-    Material option;
-    //cin >> option;
+
+    char tmp;
+    cin >> tmp;
+    Material option = static_cast<Material>(tmp);
 
     //Process option
     switch (option) {
     case air :
+      cout << "air.";
       break;
     case water :
+      cout << "water.";
       break;
     case steel :
+      cout << "steel.";
       break;
     case quit :
+      cout << "quit.";
       break;
     default :
       cout << "Invalid input";
