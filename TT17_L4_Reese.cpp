4cd8d959837da781e506efc31e9033d4c8b16ada
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-02-15 20:22:55 -0600

    added program output

--- TT17_L4_Reese.cpp
+++ TT17_L4_Reese.cpp
@@ -61,39 +61,70 @@ int main() {
     cin >> option;
     option = tolower(option);
 
-    //Check for quit
-    if (option == 'q') {
-      cout << "Goodbye.";
-      done = true;
-    }
-    
-    while (materials[option] == 0) {
+    //Material input validation
+    while (materials[option] == 0 && option != 'q') {
       cout <<"Invalid material option.\n";
       cout << menu;
       cin >> option;
       option = tolower(option);
     }
 
-    
-    //Get displacement
-    cout << "How thick is the material (in feet): ";
-    double displacement = 0;  
-    cin >> displacement;
-      
-    //Displacement input validation
-    while (displacement < 0) {
-      cout <<"Thickness must not be negative.\n";
-      cout << "How thick is the material (in feet): ";
-      cin >> displacement;
+    //Check for quit
+    if (option == 'q') {
+      cout << "Goodbye.";
+      done = true;
     }
+    
+    else {
+      //Get displacement
+      cout << "How thick is the material (in feet): ";
+      double displacement = 0;  
+      cin >> displacement;
+      
+      //Displacement input validation
+      while (displacement < 0) {
+	cout <<"Thickness must not be negative.\n";
+	cout << "How thick is the material (in feet): ";
+	cin >> displacement;
+      }
 
-    //Output time for sound to pass through material
-    double time_for = displacement / materials[option];
-    cout << "The sound will travel " << displacement << " feet in ";
-    printf("%0.4f seconds\n", time_for);
+      //Output time for sound to pass through material
+      double time_for = displacement / materials[option];
+      cout << "The sound will travel " << displacement << " feet in ";
+      printf("%0.4f seconds\n", time_for);
+    }
 
     //Seperate loops
     cout << endl;
   }
   return 0;
 }
+/*
+Program Output:
+Select a material: a)ir, w)ater, s)teel, c)opper, n)ylon, i)ron, or q)uit: a
+How thick is the material (in feet): 1100
+The sound will travel 1100 feet in 1.0000 seconds
+
+Select a material: a)ir, w)ater, s)teel, c)opper, n)ylon, i)ron, or q)uit: w
+How thick is the material (in feet): 56
+The sound will travel 56 feet in 0.0114 seconds
+
+Select a material: a)ir, w)ater, s)teel, c)opper, n)ylon, i)ron, or q)uit: s
+How thick is the material (in feet): 560000     
+The sound will travel 560000 feet in 34.1463 seconds
+
+Select a material: a)ir, w)ater, s)teel, c)opper, n)ylon, i)ron, or q)uit: a
+How thick is the material (in feet): 560000
+The sound will travel 560000 feet in 509.0909 seconds
+
+Select a material: a)ir, w)ater, s)teel, c)opper, n)ylon, i)ron, or q)uit: i
+How thick is the material (in feet): -1
+Thickness must not be negative.
+How thick is the material (in feet): 1
+The sound will travel 1 feet in 0.0001 seconds
+
+Select a material: a)ir, w)ater, s)teel, c)opper, n)ylon, i)ron, or q)uit: d
+Invalid material option.
+Select a material: a)ir, w)ater, s)teel, c)opper, n)ylon, i)ron, or q)uit: q
+Goodbye. 
+*/
