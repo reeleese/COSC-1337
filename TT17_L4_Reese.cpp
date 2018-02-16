6968124d183c68877691ad6a5266263fe39c6f59
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-02-15 18:51:06 -0600

    added menu creatio nto for loop

--- TT17_L4_Reese.cpp
+++ TT17_L4_Reese.cpp
@@ -5,7 +5,8 @@
 /*
 TODO:
 -Add purpose
--Read file contents
+-Rename data file
+-Create prompt
 */
 
 #include  <iostream>
@@ -19,6 +20,7 @@ int main() {
   string filename = "lab4_info.txt";
   ifstream speeds{filename};
 
+  //Read file into a vector file_contents
   vector<string> file_contents;
   if (speeds) {
     string item = "notAnInt";
@@ -27,57 +29,37 @@ int main() {
     }
   }
   
-  //Print menu
+  //Print menu, map materials to their respective speeds
+  string menu = "Select a material: ";
   map<char, int> options;
   for (int i = 0; i < file_contents.size(); i += 2){
+    //Print the materials and speeds
     cout << file_contents[i] << "\t" << file_contents[i+1] << endl;
+
+    //Skip table headings and map information
     if (i > 1) {
+      //Create map
       char key = tolower(file_contents[i][0]);
       int value = atoi(file_contents[i+1].c_str());
       options[key] = value;
+      
+      //Menu
+      string tmp = file_contents[i];
+      tmp.insert(1, ")");
+      menu = menu + tmp + ", ";
     }
   }
-  cout << options['i'];
-    
-
-  /*
-  //Material enum
-  enum Material {air='a', water='w', steel='s', quit='q'};
   
+  menu.resize(menu.size() -2);
   //Loop
   bool done = false;
   while (!done) {
-    //Get material
-    cout << "a)ir, w)water, s)teel or  q)uit" << endl;
-    cout << "Option: ";
-    char tmp;
-    cin >> tmp;
-    Material option = static_cast<Material>(tmp);
-
+    cout << menu << endl;
     //Get thickness
     cout << "How thick is the matarial (in feet): ";
     double thickness;
     cin >> thickness;
 
-    //Process option
-    switch (option) {
-    case air :
-      cout << "air.";
-      break;
-    case water :
-      cout << "water.";
-      break;
-    case steel :
-      cout << "steel.";
-      break;
-    case quit :
-      cout << "quit.";
-      break;
-    default :
-      cout << "Invalid input.";
-      break;
-    }
   }
-  */
   return 0;
 }
