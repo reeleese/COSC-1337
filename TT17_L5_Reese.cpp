9d829fc2d93231fbeb4f6e77f9bdd553456c1250
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-02-22 18:52:18 -0600

    records max pop for the graph height

--- TT17_L5_Reese.cpp
+++ TT17_L5_Reese.cpp
@@ -9,10 +9,34 @@ TODO:
 
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <vector>
 using namespace std;
 
+const int year0{1900};
+
 int main(){
+  //Read in people.txt
+  string file_name = "people.txt";
+  ifstream my_pop("people.txt");
   
+  //Container for each population recorded stored in chronological order
+  vector<int> populations;
+  
+  if (my_pop) {
+    string population{};
+    while (my_pop >> population)
+      populations.push_back(atoi(population.c_str()));
+  }
+  my_pop.close();
+
+  //Record maximum population
+  int max_population{0};
+  for(int population : populations)
+    if (population > max_population)
+      max_population = population;
+
+
   return 0;
 }
 
