f1442fc50ba149ec462b8b045314f955b199c78b
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-02-22 18:04:33 -0600

    Added basic framework for lab 5

---
+++ TT17_L5_Reese.cpp
@@ -0,0 +1,22 @@
+// Author: Lee Reese;  Course: COSC 1337 Spring 2018 TT17;  Instructor: Thayer
+// Lab: 4  Purpose:
+
+/*
+TODO:
+-Add purpose
+-Add program output
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+  
+  return 0;
+}
+
+/*
+Program Output:
+
+*/
