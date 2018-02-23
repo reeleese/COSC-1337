34f30459fff496f3b6d178efce9fb78bac532ba7
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-02-22 18:01:12 -0600

    Added lab5 files

--- test.cpp
+++ test.cpp
@@ -2,23 +2,10 @@
 
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <iomanip>
 using namespace std;
 
 int main() {
-  ifstream materials{"lab4_info.txt"};
-  vector<string> stuff;
-  if (materials) {
-    cout << "success.\n";
-    string speed = "yo";
-    while (materials >> speed) {
-      stuff.push_back(speed);
-    }
-  }
-  else
-    cout << "fail.";
-  for (int i = 0; i < stuff.size(); ++i) {
-    cout << stuff[i] << endl;
-  }
+  cout << setw(4)<< 3 <<" ";
   return 0;
 }
