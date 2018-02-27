0de5c5abecae40399644a27897f3e67aeb301b07
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-02-27 15:54:20 -0600

    basic framework

---
+++ TT17_L6_Reese.cpp
@@ -0,0 +1,28 @@
+// Author: Lee Reese;  Course: COSC 1337 Spring 2018 TT17;  Instructor: Thayer
+// Lab: 5  Purpose: Calculate the distance between two things
+
+/*
+TODO:
+-Purpose
+-Program output
+
+*/
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+int main() {
+  //Input data
+  cout << "Choose a thing" << endl;
+  //Determine distance (letters, numbers, points on a graph)
+
+  
+  return 0;
+}
+
+/*
+Program Output:
+
+*/
