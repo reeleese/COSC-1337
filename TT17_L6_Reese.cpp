67db0918a9658596c59d7c6ca06c4d89055edbbc
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-02-27 18:21:56 -0600

    Idk I did some things

--- TT17_L6_Reese.cpp
+++ TT17_L6_Reese.cpp
@@ -14,37 +14,36 @@ TODO:
 using namespace std;
 
 //Variables
-float distance;
-auto first;
-auto second;
+float distance{0};
+int first;
+int second;
 
 //Error Messages
-const string error_char = "";
-const string error_number = "";
-const string error_point = "";
+const string error_char = "Sorry, character must be alphabetic. Try again.";
+const string error_number = "Sorry, out-of-range. Try again.";
+const string error_point = "This error should never happen.";
 
 //Prompts
-const string prompt_char = "";
-const string prompt_number = "";
-const string prompt_point = ""
+const string prompt_char = "Gimme a character: ";
+const string prompt_number = "Gimme a number";
+const string prompt_point = "";
 
 //Input Functions
-char input(string prompt, string error_message) {
-  cout << prompt << endl;
-
-  cout << "Letter 1: ";
-  cin >> first;
+char input(bool first, string error_message) {
+  //Prompt
+  cout << (first ? "Enter first letter ":"Enter second letter ")
+       << "(a to z): ";
   
-  cout << "Letter 2: ";
-  cin >> second;
-
-  if (!(isalpha(letter_first) && isalpha(letter_second))) {
+  //Input
+  char user_input{};
+  cin >> user_input;
+  
+  //If char is not a-z or A-Z, try again.
+  if (!isalpha(user_input)) {
     cout << error_message << endl;
-    input(prompt, error_message);
+    return input(first, error_message);
   }
-
-  cout << int(first) << " : " << int(second);
-  distance = int(letter_first) - int(letter_second);
+  return user_input;
 }
 
 float input(float min, float max, string prompt, string error_message);
@@ -56,10 +55,28 @@ void input(double &x, double &y, string prompt);
 
 
 int main() {
-  cout << "Choose a thing" << endl;
+  //Sentinel
+  char option{};
   
-  cout << input("prompt", "error message");
-  
+  //Menu
+  do {
+    cout << "Options: l)etter; n)umber; p)oint; q)uit: ";
+    cin >> option;
+    
+    switch (option) {
+      case 'l':
+	first = int (input(true, error_char));
+	second = int (input(false, error_char));
+	break;
+      case 'n':
+	break;
+      case 'p':
+	break;
+      case 'q':
+	cout << "Good-bye!" << endl;
+	break;
+    }
+  } while (option != 'q');
   return 0;
 }
 
