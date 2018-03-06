86c2df205bb846fa6554dfe1d39dbaa401d6066d
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-03-06 17:56:36 -0600

    Created class Inventory with default constructo

---
+++ TT17_L7_Reese.cpp
@@ -0,0 +1,34 @@
+// Author: Lee Reese;  Course: COSC 1337 Spring 2018 TT17;  Instructor: Thayer
+// Lab: 6  Purpose: 
+
+/*
+TODO:
+-Add purpose
+
+
+
+
+
+*/
+
+#include <iostream>
+using namespace std;
+class Inventory{
+  private:
+    int itemNumber;
+    int quantity;
+    double cost;
+  public:
+    Inventory() {
+      itemNumber = 0;
+      quantity = 0;
+      cost = 0;
+    }
+
+};
+
+int main() {
+
+
+  return 0;
+}
