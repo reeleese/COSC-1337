a9bf6df6470c4bd2883961b4abf1128e71bfddef
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-03-29 19:42:59 -0500

    toString added

--- TT17_L8_Reese.cpp
+++ TT17_L8_Reese.cpp
@@ -12,6 +12,7 @@ TODO:
 #include<iostream>
 #include<string>
 #include<cmath>
+#include<sstream>
 
 using namespace std;
 class Board {
@@ -24,23 +25,37 @@ class Board {
 	{' ', ' ', ' '},
 	{' ', ' ', ' '}
       };
-    int _remainingMoves;
-    int _boardDimension;
+    int _remainingMoves = 9;
+    const int _boardDimension = 3;
 
-    //Private Functions
+    //setters
+    void subtractMove() {
+      _remainingMoves--;
+    }
   
+    //Determines if a desired position consistutes a valid position on the board
     bool getCoordinate(int &row, int &col, int position) {
+
       //Return false if  position is invalid
       if (pow(_boardDimension, 2) <= position || 0 > position)
 	return false;
 
+      //Decode position int ocoordinates
       row = position / _boardDimension;
       col = position % _boardDimension;
       return true;
     }
+
+    //Builds a square board with side length boardDimension
+    void constructBoard(int boardDimension) {
+      
+    }
   
   public:
-
+    //Constructor
+    Board() {
+      //Do nothing
+    }
     //Determine the current winner
     char winner() {
       
@@ -57,10 +72,25 @@ class Board {
       if (getCoordinate(row, col, position))
 	_board[row][col] = player;
     }
-  
+
+    //Convert Board to a string
+    string toString() {
+      stringstream ss;
+      int i = 0;
+      while(i < pow(_boardDimension, 2)) {
+	ss << "  " << i++
+	   << "| " << i++ << " |"
+	   << i++ << "  "
+	   << endl;
+	if (i != pow(_boardDimension, 2)) 
+	  ss << "------------" << endl;
+      }
+      return ss.str();
+    }
 };
 
 int main() {
-  
+  Board gameBoard = Board();
+  cout << gameBoard.toString();
   return 0;
 }
