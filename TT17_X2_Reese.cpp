d2c224ec74b6b1a8b73672e34c9cdfa56e772cea
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-04-12 20:27:34 -0500

    added thayer's comments

--- TT17_X2_Reese.cpp
+++ TT17_X2_Reese.cpp
@@ -17,14 +17,21 @@ class CoinPurse
     int _quarters, _dimes, _nickels, _pennies;
   
   public:
+    //  Step 1) Write two CoinPurse constructors:
+    //    default: set all coin counts to zero
+    //    2nd:     set all coin counts to initial values; use parameters for: penny, nickel, dime, quarter
+    //    please combine both of these into one constructor
     CoinPurse(int quarters=0, int dimes=0, int nickels=0, int pennies=0) {
+      // Step 6) avoid redundant code, call set() in constructor(s)
       set(quarters, dimes, nickels, pennies);
     }
   
+    // Step 3) Write total_value to return total value of coins in CoinPurse object
     int total_value() const {
       return _quarters*25 + _dimes*10 + _nickels*5 + _pennies*1;
     }
-  
+
+    // Step 5) Implement set() which modifies the coin counts
     bool set(int quarters, int dimes, int nickels, int pennies) {
       // If any values are negative, do nothing
       if (quarters < 0 || dimes < 0 || nickels < 0 || pennies < 0)
@@ -37,17 +44,21 @@ class CoinPurse
       _pennies = pennies;
       return true;
     }
-  
+
+    // Step 7) write show() method that outputs the number of each coin in the purse
+    //  in one string, like this: "q=4 d=3 n=2 p=1" (don't << endl;)  
     string show() const {
       return "(q=" + to_string(_quarters) +
              " d=" + to_string(_dimes) +
              " n=" + to_string(_nickels) +
              " p=" + to_string(_pennies) + ")";
     }
-
-  bool modify(int deltaQ, int deltaD, int deltaN, int deltaP) {
-    return set(_quarters + deltaQ, _dimes + deltaD,
-               _nickels + deltaN, _pennies + deltaP);
+  
+    // Step 9) write modify() method to modify coin counts using a positive (increment),
+    // negative (decrement) or zero (keep the same) value.
+    bool modify(int deltaQ, int deltaD, int deltaN, int deltaP) {
+      return set(_quarters + deltaQ, _dimes + deltaD,
+                 _nickels + deltaN, _pennies + deltaP);
   }
   
 };
@@ -57,13 +68,20 @@ void get_input(int&);
 int main() {
   cout << "COSC 1337 Exam 2 CoinPurse" << endl;
 
+  // Step 2) declare CoinPurse object called purse1;
+  // initialize with: 4 quarters, 3 dimes, 2 nickels, 1 penny
   CoinPurse purse1 = CoinPurse(4, 3, 2, 1);
+
+  // Step 4) Call the total_value method on purse1; display the result formatted as: $x.xx
   cout << '$' << static_cast<double>(purse1.total_value())/100
        << endl;
+
+  // Step 8) Call set to change values in purse1 to: 8 quarters, 7 dimes, 6 nickels, 5 pennies.
+  // Call show to display the contents of purse1
   purse1.set(8, 7, 6, 5);
   cout << purse1.show() << endl;
   
-  // Menu-Driven loop
+  // Step 10) write menu driven loop that allows the user the add or remove coins from purse1.
   bool done = false;
   while(!done) {
     // Display Menu and current sataus of purse1
@@ -109,7 +127,8 @@ int main() {
             cout << endl << error_message << endl;
           break;
         }
-
+	 
+        // Step 12 EXTRA CREDIT) add c)ents option that allows the user to add a specific amount of cents
         case 'c' : { //cents
           int cents = delta;
 
@@ -135,9 +154,8 @@ int main() {
     } //end of if-else
   } // end of menu
 
-  // Step 12 EXTRA CREDIT) add c)ents option that allows the user to add a specific amount of cents
-  
-  cout<<endl<<"Goodbye!"<<endl; // this should appear as the last line of your output
+    
+  cout<<endl<<"Goodbye!"<<endl;
   return EXIT_SUCCESS; // Use return 0 if EXIT_SUCCESS is undefined
 }
 
@@ -150,6 +168,6 @@ void get_input(int& option) {
 }
 
 /*  Step 11) Paste test output here (do this after step 12 if doing extra credit)
-
+_
 */
 
