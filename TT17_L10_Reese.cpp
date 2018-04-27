3a53869f20bfa2e03575d531769a885a213b3e6b
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-04-26 20:03:27 -0500

    added program output

--- TT17_L10_Reese.cpp
+++ TT17_L10_Reese.cpp
@@ -156,7 +156,39 @@ int main () {
 /*
 Output from Lab10 memory diagram on pointers:
 
-address of price    is: &0x22fe1c  contents of price    is: 19.95
-address of p_price  is: &0x22fe10  contents of p_price  is: 0x22fe1c
+address of price    is: &0x7ffee928488c  contents of price    is: 19.95
+address of p_price  is: &0x7ffee9284880  contents of p_price  is: 0x7ffee928488c
 The contents of *p_price is: 19.95
+
+address of PI       is: &0x7ffee9284858  contents of PI       is: 3.14159
+address of p_PI     is: &0x7ffee9284850  contents of p_PI     is: 0x7ffee9284858
+The contents of *p_PI is: 3.14159
+
+address of PI_div_2 is: &0x7ffee9284828  contents of PI_div_2 is: 1.5708
+
+address of maxSize  is: &0x7ffee9284814  contents of maxSize  is: 5028
+address of p_size   is: &0x7ffee9284808  contents of p_size   is: 0x7ffee9284814
+The contents of *p_size is: 5028
+
+address of p_amount is: &0x7ffee92847e0  contents of p_amount is: 0x7fdd97c02740
+The contents of *p_amount is: 5028
+
+address of pArray    is: &0x7ffee92847c8  contents of pArray    is: 0x7fdd97c02740
+address of pArray[0] is: &0x7fdd97c02740  contents of pArray[0] is: 11
+address of pArray[1] is: &0x7fdd97c02744  contents of pArray[1] is: 22
+address of pArray[2] is: &0x7fdd97c02748  contents of pArray[2] is: 33
+The contents of *pArray is: 11
+
+address of emptyList is: &0x7ffee9284780  contents of emptyList is: 0x0
+
+address of wizard      is: &0x7ffee9284750
+address of wizard.name is: &0x7ffee9284750  contents of wizard.name is: Gandalf
+address of wizard.next is: &0x7ffee9284768  contents of wizard.next is: 0x0
+
+address of personList          is: &0x7ffee9284718  contents of personList          is: 0x7fdd97c02750
+address of personList -> name  is: &0x7fdd97c02750  contents of personList -> name  is: Harry
+address of personList -> next  is: &0x7fdd97c02768  contents of personList -> next  is: 0x7fdd97c02770
+address of personList2         is: &0x7ffee9284710  contents of personList2         is: 0x7fdd97c02770
+address of personList2 -> name is: &0x7fdd97c02770  contents of personList2 -> name is: Sally
+address of personList2 -> next is: &0x7fdd97c02788  contents of personList2 -> next is: 0x0
 */
