7686fc7cd0282bea4b928d671a17dde4464f4d49
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-05-01 18:22:12 -0500

    DO NOT USE PREVIOUS COMMIT

--- TT17_L11_Reese.cpp
+++ TT17_L11_Reese.cpp
@@ -14,7 +14,7 @@ class Day{
   private:
     int _day;
     enum _month {jan=31, feb=28, mar=31, apr=30, may=31, jun=30,
-                 jul=31, aug=31, sep=30, oct=31, nov=30, dec=31}
+                 jul=31, aug=31, sep=30, oct=31, nov=30, dec=31};
   
   public:
     // Constructor
