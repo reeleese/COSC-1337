09e8856e0a44d598ef86281c81bb377d885c3bd2
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-05-01 18:21:45 -0500

    setting up outline for Day class

--- TT17_L11_Reese.cpp
+++ TT17_L11_Reese.cpp
@@ -10,6 +10,25 @@
 
 using namespace std;
 
+class Day{
+  private:
+    int _day;
+    enum _month {jan=31, feb=28, mar=31, apr=30, may=31, jun=30,
+                 jul=31, aug=31, sep=30, oct=31, nov=30, dec=31}
+  
+  public:
+    // Constructor
+    Day(int day = 0) {
+      if (day >= 0) _day = day;
+    }
+
+    // Return day
+    int getDay() const {
+      return _day;
+    }
+
+};
+
 int main() {
   return 0;
 }
